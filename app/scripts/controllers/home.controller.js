d875837daf0416abee214bad0e9f202f7e86efcf
kevinzeng <kevinzengdev@gmail.com>
2016-04-30 00:15:37 -0700

    updated index.html, updated home view and controller dramatically, etc

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -13,6 +13,105 @@ app.controller('HomeCtrl', ['$scope', function ($scope) {
 
 	/** Scope Objects for Home Ctrl **/
 	$scope.today = '';
+	$scope.overview = {
+		summary: {
+			title: 'Summary',
+			description: ['Experienced in Highly Complex Web Application Development, \
+						   proficient in Spring, Maven, Web Services and Front-end Technologies. \
+						   Addicted to Agile Environment and Open Source Community.',
+						   'Proficient with Responsive Web Design and Material Design'],
+			footer: 'Code, Eat, Play, Sleep, Repeat'
+		},
+		projects: [
+			{
+				title: 'Project - Blog',
+				objective: 'Blog is the my current personal website, contain profolio information, personal project brief, latest posts and more\
+							, built and tested wth cutting edge front end technology.',
+				details: [
+					{
+						description: 'Blog is Angular and Bootstrap Intensive, involved Modular Design and Material Design,\
+									  implemented popular web app components such as autofill, carousel, mdTab, pagination, modal, etc.'
+					},
+					{
+						description: 'Blog is maintained on Github Repository, deployed on the github.io using Github Pages, checkout at\
+									  the Github Link & Demo Link'
+					}
+				],
+				environment: 'Angular Web Application with Bootstrap, HTML, CSS, Material Design, Karma, Jasmine, etc',
+				links: {
+					github: 'https://github.com/jaspardzc/Blog',
+					demo: 'http://jaspardzc.github.io/blog/#/home'
+				},
+				caption: {
+					email: 'kevinzengdev@gmail',
+					timespan: 'From April 2016 - Present'
+				}
+			},
+			{
+				title: 'Project - Online Reservation System',
+				objective: '',
+				details: [
+					{
+
+					},
+					{
+
+					}
+				],
+				environment: '',
+				links: {
+					github: 'https://github.com/jaspardzc/OnlineReservationSystem',
+					demo: 'http://jaspardzc.github.io/onlinereservation/#/home'
+				},
+				caption: {
+					email: 'kevinzengdev@gmail',
+					timespan: 'From March 2015 - April 2016'
+				}
+			},
+			{
+				title: 'Project - Android Gourmet Guider',
+				objective: '',
+				details: [
+					{
+
+					},
+					{
+
+					}
+				],
+				environment: '',
+				links: {
+					github: 'https://github.com/jaspardzc/AndroidGourmetGuider',
+					demo: 'http://jaspardzc.github.io/androidgourmetguider/#/home'
+				},
+				caption: {
+					email: 'kevinzengdev@gmail',
+					timespan: 'From December 2014 - May 2015'
+				}
+			},
+			{
+				title: 'Project - Circuit Analyzer and Verilog Generator',
+				objective: '',
+				details: [
+					{
+
+					},
+					{
+
+					}
+				],
+				environment: '',
+				links: {
+					github: 'https://github.com/jaspardzc/CircuitAnalyzer',
+					demo: 'http://jaspardzc.github.io/circuitanalyzer/#/home'
+				},
+				caption: {
+					email: 'kevinzengdev@gmail',
+					timespan: 'From April 2014 - December 2014'
+				}
+			}
+		]
+	};
 
 	/** Functions for Home Ctrl **/
 	$scope.init = function() {
@@ -29,8 +128,10 @@ app.controller('HomeCtrl', ['$scope', function ($scope) {
 		$scope.today = localDateString + ' ';
 	};
 
-	$scope.isActive = function(home) {
-		//return home === $scope.home;
+	/* Function for ngClass */
+	$scope.isActive = function(index) {
+		console.log(index);
+		return index === 0;
 	};
 
 	/**  Entry Point for Home Ctrl **/
