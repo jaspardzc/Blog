254d4b4e22ae78eeb3aface91d118050a9a82869
kevinzeng <kevinzengdev@gmail.com>
2016-06-05 09:44:05 -0700

    updated login.controller

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -12,19 +12,46 @@ var app = angular.module('Blog');
 app.controller('LoginCtrl', ['$scope', '$rootScope', '$mdToast', '$timeout', '$location', '$cookies',
 					function ($scope, $rootScope, $mdToast, $timeout, $location, $cookies) {
 
-	$scope.init = function() {
+	$rootScope.admin = {
+		isAuthorized: false,
+		email: '',
+		password: '',
+		timeStamp: ''
+	};
 
+	$scope.init = function() {
+		$cookies.put("adminEmail", "kevinzengdev@gmail");
+		$cookies.put("adminPassword", "123456789");
+
+		if (angular.equals(localStorage.getItem("admin.isAuthorized"), true)) {
+			// assign the value from localStorage to $rootScope.admin object
+		}
+
+		if (angular.equals($rootScope.admin.isAuthorized, true)) {
+			$location.path('/home');
+		}
 	};
 
 	$scope.signinAsAdmin = function(admin) {
-		console.log(admin);
-		$cookies.put(1, admin.email);
-		$cookies.put(2, admin.password);
-		$cookies.put(3, admin.rememberMe);
+
+		//$cookies.put("remember", admin.rememberMe);
+		if(angular.equals($cookies.get("adminEmail"), admin.email) &&
+		   angular.equals($cookies.get("adminPassword"), admin.password)) {
+			$rootScope.admin.isAuthorized = true;
+			$rootScope.admin.email = admin.password;
+			$rootScope.admin.password = admin.password;
+			$rootScope.admin.timeStamp = new Date();
+			$rootScope.admin.timeStamp = $rootScope.admin.timeStamp.toLocaleString();
+		} else {
+			$rootScope.admin.isAuthorized = false;
+		}
 
 		if(typeof(Storage) !== "undefined") {
-			localStorage.setItem("lastname", "Smith");
-			console.log(localStorage.getItem("lastname"));
+			localStorage.setItem("admin.isAuthorized", $rootScope.admin.isAuthorized);
+			localStorage.setItem("admin.email", $rootScope.admin.email);
+			localStorage.setItem("admin.password", $rootScope.admin.password);
+			localStorage.setItem("admin.timeStamp", $rootScope.admin.timeStamp);
+			localStorage.setItem("admin.rememberMe", admin.rememberMe);
 		} else {
 			alert("Your Browser does not support HTML5 LocalStorage!");
 		}
