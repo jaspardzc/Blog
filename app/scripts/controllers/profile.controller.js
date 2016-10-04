60794f41aac49e5f504452ea91f41a546fcf06cf
kevinzeng <kevinzengdev@gmail.com>
2016-10-03 22:46:29 -0700

    POC of Morris Chart with Dynamic Data Array

--- app/scripts/controllers/profile.controller.js
+++ app/scripts/controllers/profile.controller.js
@@ -93,12 +93,109 @@ app.controller('ProfileCtrl', ['$scope', '$location',  function ($scope, $locati
 	$scope.experiences = '';
 	$scope.basic = '';
 
+	$scope.donut = '';
+	$scope.product = {
+		"selected": '',
+		"list": [
+					{
+						"name": 'QNXT',
+						"versions": [
+							{
+								"version": "5.0",
+								"count": "12"
+							},
+							{
+								"version": "4.0",
+								"count": "6"
+							},
+							{
+								"version": "3.0",
+								"count": "4"
+							},
+							{
+								"version": "2.0",
+								"count": "2"
+							}
+						]
+					},
+					{
+						"name": 'Facets',
+						"versions": [
+							{
+								"version": "5.0",
+								"count": "12"
+							},
+							{
+								"version": "4.0",
+								"count": "10"
+							},
+							{
+								"version": "3.0",
+								"count": "1"
+							},
+							{
+								"version": "2.0",
+								"count": "5"
+							}
+						]
+					},
+					{
+						"name": 'CWS',
+						"versions": [
+							{
+								"version": "5.0",
+								"count": "5"
+							},
+							{
+								"version": "4.0",
+								"count": "10"
+							},
+							{
+								"version": "3.0",
+								"count": "4"
+							},
+							{
+								"version": "2.0",
+								"count": "10"
+							}
+						]
+					}
+				]
+	};
+
+	$scope.populateChartData = function(productSelected) {
+		var array = [];
+
+		angular.forEach(productSelected.versions, function(value, key) {
+			var section = {label: value.version, value: value.count};
+			array.push(section);
+		});
+
+		return array;
+	};
+
+	$scope.selectProduct = function(index) {
+		$scope.product.selected = $scope.product.list[index];
+		var array = $scope.populateChartData($scope.product.selected);
+		$scope.donut.setData(array);
+	};
+
 	$scope.init = function() {
 		console.log($scope.profile);
 		$scope.skills = $scope.profile.skills;
 		$scope.experiences = $scope.profile.experience.professional;
 		$scope.educations = $scope.profile.education;
 		$scope.basic = $scope.profile.basic;
+
+		$scope.product.selected = $scope.product.list[0];
+
+		var array = $scope.populateChartData($scope.product.selected);
+
+		$scope.donut = new Morris.Donut({
+					  element: 'myDonut',
+					  data: array
+					});
+		console.log($scope.donut);
 	};
 
 	/* Entry Point for Profile Controller */
