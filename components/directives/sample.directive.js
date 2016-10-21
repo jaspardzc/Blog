b8a7a007e39b5335323d61cd048fa2f7d34bc387
jaspardzc <jaspardzc@gmail.com>
2016-10-21 00:27:35 -0700

    minor update

---
+++ components/directives/sample.directive.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Blog.controller:SampleDirective
+ * @description
+ * # SampleDirective
+ * Directive of the Blog Application
+ */
+var app = angular.module('Blog');
+
+
+app.directive('sampleDirective', ['$scope', function($scope) {
+
+	
+}]);
